7f2c66fa546823ff8327a7f0da2e09a0e7b2d0d2
TheTrashed <github@thetrashed.anonaddy.com>
2021-12-26 00:51:22 +0500

    Made changes to chatbot for integration with js in ui

--- chatbot.py
+++ chatbot.py
@@ -10,30 +10,33 @@ from tensorflow.keras import models
 
 from tensorflow.keras.models import load_model
 
-lemmatizer = WordNetLemmatizer()
 
-filepath = os.path.join(os.getcwd(), 'test_data')
-# print(filepath)
-with open(os.path.join(filepath, 'intents.json'), 'r') as f:
-    intents = json.loads(f.read())
+def chatbot_initialise():
+    lemmatizer = WordNetLemmatizer()
 
-with open('words.pkl', 'rb') as f:
-    words = pickle.load(f)
+    filepath = os.path.join(os.getcwd(), 'test_data')
+    with open(os.path.join(filepath, 'intents.json'), 'r') as f:
+        intents = json.loads(f.read())
 
-with open('classes.pkl', 'rb') as g:
-    classes = pickle.load(g)
+    with open('words.pkl', 'rb') as f:
+        words = pickle.load(f)
 
-model = load_model('chatbotmodel.h5')
+    with open('classes.pkl', 'rb') as g:
+        classes = pickle.load(g)
+
+    model = load_model('chatbotmodel.h5')
+
+    return lemmatizer, intents, words, classes, model
 
 
-def clean_up_sentence(sentence):
+def clean_up_sentence(lemmatizer, sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
 
 
-def bag_of_words(sentence):
-    sentence_words = clean_up_sentence(sentence)
+def bag_of_words(lemmatizer, words, sentence):
+    sentence_words = clean_up_sentence(lemmatizer, sentence)
     bag = [0] * len(words)
     for w in sentence_words:
         for i, word in enumerate(words):
@@ -43,8 +46,8 @@ def bag_of_words(sentence):
     return np.array(bag)
 
 
-def predict_class(sentence):
-    bow = bag_of_words(sentence)
+def predict_class(lemmatizer, model, sentence, classes):
+    bow = bag_of_words(lemmatizer, sentence)
     res = model.predict(np.array([bow]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
@@ -54,23 +57,20 @@ def predict_class(sentence):
 
     for r in results:
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
-    
+
     return return_list
 
-def get_response(intents_list, intents_json):
+
+def get_response(message):
+    lemmatizer, intents_json, words, classes, model = chatbot_initialise()
+
+    intents_list = predict_class(lemmatizer, model, message, classes)
+
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
         if i['tag'] == tag:
             result = random.choice(i['responses'])
             break
+
     return result
-    
-
-print('Bot is RUNNING')
-
-while True:
-    message = input("")
-    ints = predict_class(message)
-    res = get_response(ints, intents)
-    print(res)
