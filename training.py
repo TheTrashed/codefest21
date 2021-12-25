bf468062ce7e7e709ce83a8ac259d1caf6bbde22
Muhammad Ammar Raza <ammar.raza32002@gmail.com>
2021-12-25 15:28:48 +0500

    added trained files to gitignore. made and traines the neural network

--- training.py
+++ training.py
@@ -14,6 +14,7 @@ from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
+from tensorflow.python.keras.backend import dropout
 
 lemmatizer = WordNetLemmatizer()
 
@@ -82,4 +83,20 @@ train_y = list(training[:, 1])
 NEURAL NETWORK
 
 """
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.model')
+
+print('DONE')
+
 
